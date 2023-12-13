0e1951c4cfda10ad8e1676260e9bd7d1222ee7f1
Mera Fares <fare2170@mylaurier.com>
2023-12-12 22:47:58 -0500

    deleted:    src/logo.svg 	deleted:    src/reportWebVitals.js 	deleted:    src/setupTests.js

---
+++ src/Components/VideoList.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import { Grid } from "@material-ui/core";
+
+import VideoItem from "./Videoitem";
+
+const VideoList = ({ videos, onVideoSelect }) => {
+    const listOfVideos = videos.map((video, id) => <VideoItem onVideoSelect={onVideoSelect} key={id} video={video} />)
+
+
+    return (
+        <Grid container spacing={10}>
+
+            {listOfVideos}
+
+        </Grid>
+    )
+        
+}
+
+export default VideoList;
